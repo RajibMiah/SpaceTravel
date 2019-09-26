8c634b6ac1e1be623353a67f9664f3e5fce6a038
RajibMiah <51170219+RajibMiah@users.noreply.github.com>
2019-09-26 13:40:45 -0700

    voyger 1 path

---
+++ Assets/TypeWriterText.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class TypeWriterText : MonoBehaviour
+{
+    Text txt;
+    string story;
+    public float waiting = 0.125f;
+    private AudioSource audioS;
+    void Awake() {
+        txt = GetComponent<Text>();
+        story = txt.text;
+        txt.text = "";
+        audioS = GetComponent<AudioSource>();
+        // TODO: add optional delay when to start
+        StartCoroutine("PlayText");
+        
+    }
+
+    IEnumerator PlayText() {
+        if (audioS != null && !audioS.enabled) {
+            audioS.enabled = true;
+        }
+        foreach (char c in story) {
+            txt.text += c;
+            yield return new WaitForSeconds(waiting);
+        }
+        if(audioS != null && audioS.enabled) {
+            audioS.enabled = false;
+        }
+    }
+
+}
---
+++ Assets/Voyger1SpeedControl.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PathCreation;
+
+public class Voyger1SpeedControl : MonoBehaviour
+{
+    public float WheninTheCollider;
+    public float whenOutOfCollider;
+
+    private bool isEnter;
+    private bool isExit;
+
+    public PathCreator pathCreator;
+    public EndOfPathInstruction endOfPathInstruction;
+    public float speed = 5;
+    float distanceTravelled;
+
+    private void Awake()
+    {
+        var boxCollider = gameObject.AddComponent<BoxCollider>();
+        isEnter = true;
+        isExit = true;
+
+    }
+
+    void Update()
+    {
+        if (pathCreator != null)
+        {
+            distanceTravelled += speed * Time.deltaTime;
+            transform.position = pathCreator.path.GetPointAtDistance(distanceTravelled, endOfPathInstruction);
+            // transform.rotation = pathCreator.path.GetRotationAtDistance(distanceTravelled, endOfPathInstruction);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (isEnter)
+        {
+            speed = WheninTheCollider;
+            Debug.Log("in");
+            isEnter = false;
+            isExit = true;
+        }
+
+    }
+
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (isExit)
+        {
+            speed = whenOutOfCollider;
+            Debug.Log("Exit");
+            isExit = false;
+            isEnter = true;
+        }
+
+
+    }
+}
